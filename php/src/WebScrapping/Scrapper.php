b0330d0ba55a0a40575e10e9f67f312cb02ecb41
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-29 17:27:06 -0300

    Configurando o método scrap para a criação do objeto Person de acordo com os dados do arquivo HTML.

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -10,38 +10,58 @@ use Chuva\Php\WebScrapping\Entity\Person;
  */
 
 
- 
+
 class Scrapper
 {
-  
-
-private function getElementsByClassName($dom, $ClassName, $tagName=null) {
-    if($tagName){
-        $Elements = $dom->getElementsByTagName($tagName);
-    }else {
-        $Elements = $dom->getElementsByTagName("*");
-    }
-    $Matched = array();
-    for($i=0;$i<$Elements->length;$i++) {
-        if($Elements->item($i)->attributes->getNamedItem('class')){
-            if($Elements->item($i)->attributes->getNamedItem('class')->nodeValue == $ClassName) {
-                $Matched[]=$Elements->item($i);
-            }
-        }
-    }
-    return $Matched;
-}
 
   /**
-   * Loads paper information from the HTML and returns the array with the data.
+   * Loads paper information from the HTML and returns the array with the Element.
    */
   public function scrap(\DOMDocument $dom): array
   {
-      $papers = [];
-      $rows = $this->getElementsByClassName($dom, 'row', 'div');
+    $xPATH = new \DOMXPath($dom);
+    $papers = [];
+    $authors = [];
 
-     
 
+    $titleElements = $xPATH->query('//*[@class="my-xs paper-title"]');
+    $authorsElements = $xPATH->query('//*[@class="authors"]');
+    $idElements = $xPATH->query(('//*[@class="volume-info"]'));
+    $postTypeElements = $xPATH->query(('//*[@class="tags mr-sm"]'));
+
+    foreach ($authorsElements as $authorElement) {
+      foreach ($authorElement->getElementsByTagName('span') as $authorsSpan) {
+        // Obtém o texto do atributo 'title' que contém informações sobre a universidade
+        $authorInstitution = '';
+        if ($authorsSpan->hasAttribute('title')) {
+          $authorInstitution = $authorsSpan->getAttribute('title');
+        }
+
+        // Obtém o texto do conteúdo do 'span', que contém o nome do autor
+        $authorName = trim($authorsSpan->textContent);
+
+        // Cria uma nova instância de Person com o nome do autor e a universidade
+        $author = new Person($authorName, $authorInstitution);
+
+        // Adiciona o autor ao array de autores
+        $authors[] = $author;
+       
+      }
+    }
+
+    return $papers;
   }
 
+
+
+
 }
+
+
+
+
+
+
+
+
+
