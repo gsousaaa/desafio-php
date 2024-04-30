92326c5c9a84221cd819ceeca0db593e54f62ee9
gsousaaa <glaucoezequiel94@gmail.com>
2024-04-30 12:18:24 -0300

    Implementando o método para criação de um Paper.

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -21,35 +21,48 @@ class Scrapper
   {
     $xPATH = new \DOMXPath($dom);
     $papers = [];
-    $authors = [];
+    $authorsByPostId = [];
 
 
     $titleElements = $xPATH->query('//*[@class="my-xs paper-title"]');
     $authorsElements = $xPATH->query('//*[@class="authors"]');
     $idElements = $xPATH->query(('//*[@class="volume-info"]'));
     $postTypeElements = $xPATH->query(('//*[@class="tags mr-sm"]'));
+   
+    for ($i = 0; $i < $idElements->length; $i++) {
+      $postId = $idElements->item($i)->textContent;
+      $title = $titleElements->item($i)->textContent;
+      $type = $postTypeElements->item($i)->textContent;
 
-    foreach ($authorsElements as $authorElement) {
-      foreach ($authorElement->getElementsByTagName('span') as $authorsSpan) {
-        // Obtém o texto do atributo 'title' que contém informações sobre a universidade
+      //verifica se o PostId já existe no array, se não, cria um novo array vazio
+      if (!isset($authorsByPostId[$postId])) {
+        $authorsByPostId[$postId] = [];
+    }
+
+
+      foreach ($authorsElements->item($i)->getElementsByTagName('span') as $authorsSpan) {
         $authorInstitution = '';
         if ($authorsSpan->hasAttribute('title')) {
           $authorInstitution = $authorsSpan->getAttribute('title');
         }
 
-        // Obtém o texto do conteúdo do 'span', que contém o nome do autor
         $authorName = trim($authorsSpan->textContent);
 
-        // Cria uma nova instância de Person com o nome do autor e a universidade
-        $author = new Person($authorName, $authorInstitution);
+        $author = new Person($authorName, $authorInstitution, $postId);
 
-        // Adiciona o autor ao array de autores
-        $authors[] = $author;
-       
+        if ($author->postId !== $postId) {
+          throw new \Exception('O autor não pertence a esse POST');
+        }
+
+        // Adiciona o autor correspondente a chave do array associativo, usando postId como chave
+        $authorsByPostId[$postId][] = $author;
+
       }
-    }
 
-    return $papers;
+      $papers[] = new Paper($postId, $title, $type, $authorsByPostId[$postId]);
+
+    }
+    return array_filter($papers);
   }
 
 
